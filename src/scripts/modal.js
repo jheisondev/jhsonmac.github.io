75998b7864234b0858ba6fe41db1205e3d887128
Jheison Maciel Ines <jheisonmaciel@hotmail.com>
2019-12-02 22:50:44 -0300

    Add components

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -13,8 +13,28 @@ function iniciaModal(modalId) {
 const button = document.querySelector('.modal-more');
 button.addEventListener('click', () => iniciaModal('modal-container'));
 
+// PORTIFOLIO 
+
 const modal_portifolio = document.querySelector('.mp-1');
-modal_portifolio.addEventListener('click', () => iniciaModal('modal-container-portifolio'));
+modal_portifolio.addEventListener('click', () => iniciaModal('modal-container-portifolio-1'));
+
 
 const modal_portifolio2 = document.querySelector('.mp-2');
 modal_portifolio2.addEventListener('click', () => iniciaModal('modal-container-portifolio-2'));
+
+
+const modal_portifolio3 = document.querySelector('.mp-3');
+modal_portifolio3.addEventListener('click', () => iniciaModal('modal-container-portifolio-3'));
+
+
+const modal_portifolio4 = document.querySelector('.mp-4');
+modal_portifolio4.addEventListener('click', () => iniciaModal('modal-container-portifolio-4'));
+
+
+const modal_portifolio5 = document.querySelector('.mp-5');
+modal_portifolio5.addEventListener('click', () => iniciaModal('modal-container-portifolio-5'));
+
+
+const modal_portifolio6 = document.querySelector('.mp-6');
+modal_portifolio6.addEventListener('click', () => iniciaModal('modal-container-portifolio-6'));
+
